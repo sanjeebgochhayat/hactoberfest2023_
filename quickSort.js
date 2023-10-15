209c96f09657c08c578b4bb1bd4ee8bc9ec9aabf
sanjeebgochhayat <133559804+sanjeebgochhayat@users.noreply.github.com>
2023-10-15 09:09:10 +0530

    Create quickSort.js
    quick sort in js

---
+++ quickSort.js
@@ -0,0 +1,23 @@
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const unsortedArray = [6, 1, 8, 3, 9, 2];
+const sortedArray = quickSort(unsortedArray);
+console.log("Sorted Array:", sortedArray);
